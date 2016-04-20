ac213d42f2e48eb6615cee7753d767f6af256838
Jonathan Sterling <jon@jonmsterling.com>
2016-04-20 13:09:56 -0700

    add smart constructors; add Ord instance

--- src/Data/Json/Extended.purs
+++ src/Data/Json/Extended.purs
@@ -6,6 +6,21 @@ module Data.Json.Extended
   , roll
   , unroll
 
+  , null
+  , boolean
+  , integer
+  , decimal
+  , string
+  , timestamp
+  , date
+  , time
+  , interval
+  , objectId
+  , orderedSet
+  , object
+  , object'
+  , array
+
   , renderEJson
 
   , arbitraryEJsonOfSize
@@ -15,11 +30,17 @@ module Data.Json.Extended
 import Prelude
 
 import Data.Eq1 (eq1)
+import Data.Ord1 (compare1)
 import Data.Argonaut.Encode (class EncodeJson, encodeJson)
 import Data.Argonaut.Decode (class DecodeJson, decodeJson)
 
 import Data.Functor.Mu as Mu
+import Data.HugeNum as HN
+import Data.List as L
+import Data.Map as Map
 import Data.Maybe as M
+import Data.StrMap as SM
+import Data.Tuple as T
 
 import Test.StrongCheck as SC
 import Test.StrongCheck.Gen as Gen
@@ -53,6 +74,10 @@ unroll =
 instance eqEJson ∷ Eq EJson where
   eq (EJson a) (EJson b) =
     eq1 (Mu.unroll a) (Mu.unroll b)
+
+instance ordEJson ∷ Ord EJson where
+  compare (EJson a) (EJson b) =
+    compare1 (Mu.unroll a) (Mu.unroll b)
 
 instance showEJson ∷ Show EJson where
   show = renderEJson
@@ -115,3 +140,46 @@ renderEJson (EJson x) =
     renderEJson
     (EJson <$> Mu.unroll x)
 
+null ∷ EJson
+null = roll Sig.Null
+
+boolean ∷ Boolean → EJson
+boolean = roll <<< Sig.Boolean
+
+integer ∷ Int → EJson
+integer = roll <<< Sig.Integer
+
+decimal ∷ HN.HugeNum → EJson
+decimal = roll <<< Sig.Decimal
+
+string ∷ String → EJson
+string = roll <<< Sig.String
+
+timestamp ∷ String → EJson
+timestamp = roll <<< Sig.Timestamp
+
+date ∷ String → EJson
+date = roll <<< Sig.Date
+
+time ∷ String → EJson
+time = roll <<< Sig.Time
+
+interval ∷ String → EJson
+interval = roll <<< Sig.Interval
+
+objectId ∷ String → EJson
+objectId = roll <<< Sig.ObjectId
+
+orderedSet ∷ Array EJson → EJson
+orderedSet = roll <<< Sig.OrderedSet
+
+array ∷ Array EJson → EJson
+array = roll <<< Sig.Array
+
+object ∷ Map.Map EJson EJson → EJson
+object = roll <<< Sig.Object <<< L.fromList <<< Map.toList
+
+object' ∷ SM.StrMap EJson → EJson
+object' = roll <<< Sig.Object <<< map go <<< L.fromList <<< SM.toList
+  where
+    go (T.Tuple a b) = T.Tuple (string a) b
