0bb569e7477169b936d39fcf059dfebc8d03d2b6
Jonathan Sterling <jonsterling@users.noreply.github.com>
2016-04-22 11:57:51 -0700

    SD-1575: add parser (#2)
    * Add parser
    
    * add parse-render-round-trip property test; fix bugs

--- test/Main.purs
+++ test/Main.purs
@@ -9,7 +9,9 @@ import Control.Monad.Eff.Console (CONSOLE)
 import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Encode (encodeJson)
 import Data.Either as E
-import Data.Json.Extended (EJson, arbitraryJsonEncodableEJsonOfSize)
+import Data.Json.Extended (EJson, arbitraryJsonEncodableEJsonOfSize, arbitraryEJsonOfSize, renderEJson, parseEJson)
+
+import Text.Parsing.Parser as P
 
 import Test.StrongCheck as SC
 
@@ -19,14 +21,30 @@ type TestEffects =
   , console ∷ CONSOLE
   )
 
+newtype ArbJsonEncodableEJson = ArbJsonEncodableEJson EJson
 newtype ArbEJson = ArbEJson EJson
 
-instance arbitraryArbEJson ∷ SC.Arbitrary ArbEJson where
-  arbitrary = ArbEJson <$> arbitraryJsonEncodableEJsonOfSize 2
+instance arbitraryArbJsonEncodableEJson ∷ SC.Arbitrary ArbJsonEncodableEJson where
+  arbitrary = ArbJsonEncodableEJson <$> arbitraryJsonEncodableEJsonOfSize 2
 
-main :: Eff TestEffects Unit
-main = do
-  SC.quickCheck \(ArbEJson x) →
+instance arbitraryArbEJson ∷ SC.Arbitrary ArbEJson where
+  arbitrary = ArbEJson <$> arbitraryEJsonOfSize 2
+
+testJsonSerialization ∷ Eff TestEffects Unit
+testJsonSerialization =
+  SC.quickCheck \(ArbJsonEncodableEJson x) →
     case decodeJson (encodeJson x) of
       E.Right y → x == y SC.<?> "Mismatch:\n" <> show x <> "\n" <> show y
       E.Left err → SC.Failed $ "Parse error: " <> err
+
+testRenderParse ∷ Eff TestEffects Unit
+testRenderParse =
+  SC.quickCheck \(ArbEJson x) →
+    case P.runParser (renderEJson x) parseEJson of
+      E.Right y → x == y SC.<?> "Mismatch:\n" <> show x <> "\n" <> show y
+      E.Left err → SC.Failed $ "Parse error: " <> show err <> " when parsing:\n\n " <> renderEJson x <> "\n\n"
+
+main :: Eff TestEffects Unit
+main = do
+  testJsonSerialization
+  testRenderParse
